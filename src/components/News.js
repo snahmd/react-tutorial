2b500e3a7a36ebbed2403fbc5006986d005d125b
snahmd <sanahmed7@gmail.com>
2020-03-05 23:17:47 +0100

    using props 3

--- src/components/News.js
+++ src/components/News.js
@@ -3,17 +3,15 @@ import NewsItem from './Newsitem';
 
 class News extends React.Component {
   render(){
-    
+    const elements = this.props.news.map(news => 
+      <NewsItem 
+      title={news.title}
+      description={news.description}/>
+      )
 
     return(
       <div> 
-        {
-          this.props.news.map(news => 
-            <NewsItem 
-            title={news.title}
-            description={news.description}/>
-            )
-        }
+        { elements }
       </div>
     )
   }
