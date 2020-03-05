447e4864bf9cadb600e7f7c7f09bcb818f1b9db2
snahmd <sanahmed7@gmail.com>
2020-03-05 23:13:44 +0100

    using props 2

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,22 @@ import News from './News';
 
 class App extends Component {
   render() {
+    const news = [{
+      title: "title 1",
+      description: "description 1",
+    },
+    {
+      title: "title 2",
+      description: "description 2"
+    },
+    {
+      title: "title 3",
+      description: "description 3" 
+    }];
     return(
       <div className="App">
         <header className="App-header">
-        <News/>
+        <News news={news}/>
         
         </header>
        
--- src/components/News.js
+++ src/components/News.js
@@ -3,30 +3,17 @@ import NewsItem from './Newsitem';
 
 class News extends React.Component {
   render(){
-    const news = [{
-        title: "title 1",
-        description: "description 1",
-      },
-      {
-        title: "title 2",
-        description: "description 2"
-      },
-      {
-        title: "title 3",
-        description: "description 3" 
-    }];
+    
 
     return(
       <div> 
-        <NewsItem 
-          title={news[0].title}
-          description={news[0].description}/>
-        <NewsItem 
-          title={news[1].title}
-          description={news[1].description}/>
-        <NewsItem 
-          title={news[2].title}
-          description={news[2].description}/>
+        {
+          this.props.news.map(news => 
+            <NewsItem 
+            title={news.title}
+            description={news.description}/>
+            )
+        }
       </div>
     )
   }
