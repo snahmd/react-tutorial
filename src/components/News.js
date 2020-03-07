9699a4b59b21f405884d4827d25ad09684e54b8f
snahmd <sanahmed7@gmail.com>
2020-03-07 22:58:47 +0100

    adding propTypes shape

--- src/components/News.js
+++ src/components/News.js
@@ -14,8 +14,7 @@ class News extends React.Component {
     const elements = this.props.news.map(news => 
       <div key={news.id}>
         <NewsItem 
-          title={news.title}
-          description={news.description}/>
+          newsData={news}/>
       </div>
       );
 
