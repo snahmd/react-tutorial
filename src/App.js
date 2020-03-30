2e0655b6a434eba48e788a43c6d980f6ed690b9a
snahmd <sanahmed7@gmail.com>
2020-03-30 22:05:10 +0200

    connect component to store

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
+import { connect } from 'react-redux';
 
 class App extends Component {
-
-  
-
-  
-
   render() {
   
     return(
@@ -21,4 +16,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
